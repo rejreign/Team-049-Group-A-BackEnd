41bba1557bb87124e32c428b5889be1bcf1aab16
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-08-28 03:51:33 +0100

    Chore(Router): Add routing for 'not-found' routes

--- app/router.js
+++ app/router.js
@@ -9,4 +9,14 @@ const BaseController = require("./controllers/BaseController");
 
 router.get("/", BaseController.renderData);
 
+// not found route
+router.use((req, res, next) => {
+  if (!req.route) {
+    res.status(404).json({
+      status: "not-found",
+      message: "The page you're looking for was not found",
+    });
+  }
+});
+
 module.exports = router;
